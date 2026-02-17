22bf523474596620100b54a5776ac9978f96c9ed
--list <alexandr.chaban.65@gmail.com>
2026-02-17 20:13:08 +0200

    solved the task from section 2.8

--- scripts/script.js
+++ scripts/script.js
@@ -4,3 +4,11 @@ let admin = name;
 
 let userName = prompt("What is your name?");
 alert(`Hello ${userName}`);
+
+//2.8 task, fix the addition
+let a = prompt("First number?", 1);
+let b = prompt("Second number?", 2);
+// prompt return string by default, as a result "+" operator will concatenate 2 string
+alert(a + b); // 12
+// should use type conversion to Number
+alert(+a + +b);
